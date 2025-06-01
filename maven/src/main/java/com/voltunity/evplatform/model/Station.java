aa8227a8c14ec59547d1ee28e29a1e3bda1a3ef2
mackdiogo95 <dmcg@ua.pt>
2025-06-01 19:13:04 +0100

    CRM-20 <Registo de novas estacoes>

--- maven/src/main/java/com/voltunity/evplatform/model/Station.java
+++ maven/src/main/java/com/voltunity/evplatform/model/Station.java
@@ -1,15 +1,15 @@
 package com.voltunity.evplatform.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import java.util.List;
 
 @Entity
 public class Station {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
     private String stationStatus;
     private float lat;
@@ -18,10 +18,12 @@ public class Station {
     private int totalSlots;
     private float maxPower;
 
-    // Construtor padrão
+    @ElementCollection
+    private List<String> chargerTypes;
+
     public Station() {}
-    // Construtor com parâmetros
-    public Station(String name, String stationStatus, float lat, float lng, String address, int totalSlots, float maxPower) {
+
+    public Station(String name, String stationStatus, float lat, float lng, String address, int totalSlots, float maxPower, List<String> chargerTypes) {
         this.name = name;
         this.stationStatus = stationStatus;
         this.lat = lat;
@@ -29,23 +31,79 @@ public class Station {
         this.address = address;
         this.totalSlots = totalSlots;
         this.maxPower = maxPower;
+        this.chargerTypes = chargerTypes;
     }
-    
+
     // Getters e Setters
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
-    public String getStationStatus() { return stationStatus; }
-    public void setStationStatus(String stationStatus) { this.stationStatus = stationStatus; }
-    public float getLat() { return lat; }
-    public void setLat(float lat) { this.lat = lat; }
-    public float getLng() { return lng; }
-    public void setLng(float lng) { this.lng = lng; }
-    public String getAddress() { return address; }
-    public void setAddress(String address) { this.address = address; }
-    public int getTotalSlots() { return totalSlots; }
-    public void setTotalSlots(int totalSlots) { this.totalSlots = totalSlots; }
-    public float getMaxPower() { return maxPower; }
-    public void setMaxPower(float maxPower) { this.maxPower = maxPower; }
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStationStatus() {
+        return stationStatus;
+    }
+
+    public void setStationStatus(String stationStatus) {
+        this.stationStatus = stationStatus;
+    }
+
+    public float getLat() {
+        return lat;
+    }
+
+    public void setLat(float lat) {
+        this.lat = lat;
+    }
+
+    public float getLng() {
+        return lng;
+    }
+
+    public void setLng(float lng) {
+        this.lng = lng;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getTotalSlots() {
+        return totalSlots;
+    }
+
+    public void setTotalSlots(int totalSlots) {
+        this.totalSlots = totalSlots;
+    }
+
+    public float getMaxPower() {
+        return maxPower;
+    }
+
+    public void setMaxPower(float maxPower) {
+        this.maxPower = maxPower;
+    }
+
+    public List<String> getChargerTypes() {
+        return chargerTypes;
+    }
+
+    public void setChargerTypes(List<String> chargerTypes) {
+        this.chargerTypes = chargerTypes;
+    }
 }
