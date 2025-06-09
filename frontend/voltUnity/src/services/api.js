c529404fd4b523f82621e6569a183fc7ec5be92e
Giovanni Santos <giovanni.s@ua.pt>
2025-06-09 17:28:36 +0100

    Update api.js

--- frontend/voltUnity/src/services/api.js
+++ frontend/voltUnity/src/services/api.js
@@ -2,7 +2,7 @@
 
 import axios from 'axios';
 
-const API_BASE_URL = 'http://springboot-app:8080/api/v1';
+const API_BASE_URL = 'http://localhost:8080/api/v1';
 
 export const api = axios.create({
     baseURL: API_BASE_URL,
