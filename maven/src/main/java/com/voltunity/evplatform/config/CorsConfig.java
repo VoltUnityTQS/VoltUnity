b6a8dee76508de2c11e0d6383915a0fed4ef3b71
Giovanni Santos <giovanni.s@ua.pt>
2025-06-09 21:17:26 +0100

    properties

--- maven/src/main/java/com/voltunity/evplatform/config/CorsConfig.java
+++ maven/src/main/java/com/voltunity/evplatform/config/CorsConfig.java
@@ -6,13 +6,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.beans.factory.annotation.Value;
 
 @Configuration
 public class CorsConfig {
-
-    @Value("${frontend.end}")
-    private String frontend_host;
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
@@ -20,7 +16,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins(frontend_host, "http://localhost")
+                        .allowedOrigins("http://deti-tqs-16", "http://localhost")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(false);
